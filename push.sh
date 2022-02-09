6a5ce72446f9b57d9f0a7bf502db117402984dac
Chenow <ant.cheneau@hotmail.fr>
2022-02-09 20:24:04 +0000

    test

---
+++ push.sh
@@ -0,0 +1,8 @@
+cd IA_tri_dechets
+
+git config --global user.email "ant.cheneau@hotmail.fr"
+git config --global user.name "Chenow"
+git add -A
+git commit -m "test"
+
+git push --set-upstream https://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@github.com/Chenow/IA_tri_dechets.git master
