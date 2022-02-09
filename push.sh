dcdf5a085b3c8991f955346f159646d91d71929e
Chenow <ant.cheneau@hotmail.fr>
2022-02-09 21:10:26 +0000

    test

--- push.sh
+++ push.sh
@@ -5,4 +5,4 @@ git config --global user.name "Chenow"
 git add -A
 git commit -m "test"
 
-git push https://x-acess-token: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@github.com/Chenow/IA_tri_dechets.git 
+git push https://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@github.com/Chenow/IA_tri_dechets.git 
